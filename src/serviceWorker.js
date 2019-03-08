4b1fd31d98d0d8a1e89407828a915f9b778f679c
Benson Maruchu <benmaruchu@gmail.com>
2019-03-08 19:04:39 +0300

    chore(configs): add prettier and eslint rules

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -10,6 +10,7 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read http://bit.ly/CRA-PWA
 
+/* eslint-disable */
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
@@ -133,3 +134,4 @@ export function unregister() {
     });
   }
 }
+/* eslint-enable */
